35edb50c1f0f7d03ab7d8fa662be636c7c231715
Umar-37 <omaralhussan@outlook.com>
2022-03-19 09:27:52 +0300

    base repo

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+	<h1 class="bg-yellow-200" >Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+
+<style global lang="postcss">
+	h1{
+		@apply bg-indigo-500;
+		@apply bg-red-500;
+		@apply bg-green-500;
+		@apply text-center;
+	}
+</style>
